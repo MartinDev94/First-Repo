fea7342cfa4acaf234cf96cb77f37ef989b6e102
Martin Petrosyan <petrosyanmartin21@gmail.com>
2022-03-08 17:10:45 +0400

    First-Commit

---
+++ Xndir1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xndir1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] nums = { { 1, 2, 6, 5 }, { 8, 7, 12, 13 }, { 9, 12, 10, 11 }, { 14, 3, 15, 4 }, { 20, 60, 40, 33 } };
+            int columns = nums.GetUpperBound(0) + 1;
+            int columLeng = nums.Length / columns;
+            int[] result = new int[columns];
+            for (int i = 0; i < columns; i++)
+            {
+                int max = nums[i, 0];
+                for (int j = 1; j < columLeng; j++)
+                {
+                    if(max < nums[i, j])
+                    {
+                        max = nums[i, j];
+                    }
+                }
+                result[i] = max;
+            }
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
