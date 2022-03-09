eae6b6aab752f0925fa6fe2e6ca2bda7308280a5
Martin Petrosyan <petrosyanmartin21@gmail.com>
2022-03-09 10:47:37 +0400

    Add new task

--- Xndir1/Program.cs
+++ Xndir1/Program.cs
@@ -8,8 +8,21 @@ namespace Xndir1
 {
     class Program
     {
+        public static bool IsPalindrom(string word)
+        {
+            for (int i = 0; i < word.Length/2; i++)
+            {
+                if(word[i] != word[word.Length -1 -i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine(IsPalindrom("barbados"));
             int[,] nums = { { 1, 2, 6, 5 }, { 8, 7, 12, 13 }, { 9, 12, 10, 11 }, { 14, 3, 15, 4 }, { 20, 60, 40, 33 } };
             int columns = nums.GetUpperBound(0) + 1;
             int columLeng = nums.Length / columns;
@@ -19,7 +32,7 @@ namespace Xndir1
                 int max = nums[i, 0];
                 for (int j = 1; j < columLeng; j++)
                 {
-                    if(max < nums[i, j])
+                    if (max < nums[i, j])
                     {
                         max = nums[i, j];
                     }
